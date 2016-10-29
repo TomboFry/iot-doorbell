f7fd5e41bdd6e88664eb4316db6829648473ce46
Jack Hair <jackhair@outlook.com>
2016-10-29 20:26:34 +0000

    Allow the adding and viewing of users

--- src/server.py
+++ src/server.py
@@ -1,11 +1,10 @@
 import time, pymongo, os
 from twilio.rest import TwilioRestClient
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 client = MongoClient()
-db = client.doorbell
-dings = db.dings
+db = client.doorbell 
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,14 +16,36 @@ def handle_ring():
     ding()
     return "D00T"
 
+@app.route('/settings/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'POST':
+        new_user = User(request.form['name'], request.form['number'], request.form['email'])
+        new_user.save()
+    return render_template('settings-users.html', users=get_users())
+
 def ding():
     post = {"Time": time.time()}
-    dings.insert_one(post)
+    db.dings.insert_one(post)
     notify()
 
 def notify():
     return 0;
 
+def get_users():
+    users = []
+    for user in db.users.find():
+        users.append(User(user["Name"],user["Number"],user["Email"]))
+    return users
+
+class User(object):
+    def __init__(self, name, number, email):
+        self.name = name
+        self.number = number
+        self.email = email
+
+    def save(self):
+        post = {"Name": self.name, "Number": self.number, "Email" : self.email}
+        db.users.insert_one(post)
     
 class Phone(object):
     def __init__(self):
